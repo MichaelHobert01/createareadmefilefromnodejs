8f0ae274d85defca6c276caf41c03e6855fe7fde
Michael Hobert <michaelhobertjr315@gmail.com>
2023-04-09 23:46:57 -0400

    new commit for red blog

---
+++ README.md
@@ -0,0 +1,41 @@
+ ## Table of Contents 
+
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contrbuting](#contributing)
+5. [Tests](#tests)
+6. [License](#license)
+
+# Cliffords Red Blog
+
+## Description 
+
+ Hanging out with my dawgs
+
+## Installation 
+
+ Click install button and wait for installation
+
+## Usage 
+
+ Don't
+
+## Contribution Guidelines 
+
+ To contribute, add something
+
+## Testing Instructions 
+
+ Use keykll
+
+## License 
+
+ ![License](https://img.shields.io/badge/license-29302940-blue.svg)
+
+ This application is covered under the 29302940 license.
+
+##Questions
+
+If you have any questions or comments about the application, please contact me at mjso2w08049. You can also visit my Github profile (https://github.com/wiwoisosisp)to see my other projects.
+
--- index.js
+++ index.js
@@ -1,66 +1,90 @@
-const md = require ("markdown-it");
+const md = require("markdown-it");
 const fs = require("fs");
 const inquirer = require('inquirer');
 inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What will your project title be?",
-      name: "projectTitle",
-    },
-    {
-      type: "input",
-      message: "What will your description be?",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "what are your installation instructions",
-      name: "installationInstructions",
-    },
-    {
-      type: "input",
-      message: "What will the usage info be?",
-      name: "usageInfo",
-    },
-    {
-      type: "input",
-      message: "What will the contribution guidelines be?",
-      name: "contributeGuide",
-    },
-    {
-      type: "input",
-      message: "What are the test instructions",
-      name: "testInstruct",
-    },
-    {
-      type: "input",
-      message: "What will the license badge be?",
-      name: "license",
-    },
-  ])
-  .then((answers) => {
-    let readme = `# ${answers.projectTitle}\n\n`;
-    readme += `## Description\n\n ${answers.description}\n\n`;
-    readme += `## Installation\n\n ${answers.installationInstructions}\n\n`;
-    readme += `## Usage\n\n ${answers.usageInfo}\n\n`;
-    readme += `## Contribution Guidelines\n\n ${answers.contributeGuide}`;
-    readme += `## Testing Instructions\n\n ${answers.testInstruct}\n\n`;
+    .prompt([
+        {
+            type: "input",
+            message: "What will your project title be?",
+            name: "projectTitle",
+        },
+        {
+            type: "input",
+            message: "What will your description be?",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "what are your installation instructions",
+            name: "installationInstructions",
+        },
+        {
+            type: "input",
+            message: "What will the usage info be?",
+            name: "usageInfo",
+        },
+        {
+            type: "input",
+            message: "What will the contribution guidelines be?",
+            name: "contributeGuide",
+        },
+        {
+            type: "input",
+            message: "What are the test instructions",
+            name: "testInstruct",
+        },
+        {
+            type: "input",
+            message: "What will the license badge be?",
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "What will the email be?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What will the github username be?",
+            name: "githubUsername",
+        },
+    ])
+    .then((answers) => {
 
-    let licenseBadge = "";
-    let licenseNotice = "";
+        let tableOfContents = `## Table of Contents \n\n`;
 
-    if (answers.license !== "") {
-      licenseBadge = `![License](https://img.shields.io/badge/license-${answers.license}-blue.svg)\n\n`;
-      licenseNotice = `This application is covered under the ${answers.license} license.\n\n`;
-    }
+        tableOfContents += `1. [Description](#description)\n`;
+        tableOfContents += `2. [Installation](#installation)\n`;
+        tableOfContents += `3. [Usage](#usage)\n`;
+        tableOfContents += `4. [Contrbuting](#contributing)\n`;
+        tableOfContents += `5. [Tests](#tests)\n`;
+        tableOfContents += `6. [License](#license)\n\n`;
 
-    readme += `## License \n\n ${licenseBadge} ${licenseNotice}`;
 
-    const fs = import("fs");
+        let readme = ` ${tableOfContents}# ${answers.projectTitle}\n\n`;
+        readme += `## Description \n\n ${answers.description}\n\n`;
+        readme += `## Installation \n\n ${answers.installationInstructions}\n\n`;
+        readme += `## Usage \n\n ${answers.usageInfo}\n\n`;
+        readme += `## Contribution Guidelines \n\n ${answers.contributeGuide}\n\n`;
+        readme += `## Testing Instructions \n\n ${answers.testInstruct}\n\n`;
 
-    fs.writeFile("README.md", readme ,(err) => {
-      if (err) throw err;
-      console.log("README.md file has been generated!");
+        let licenseBadge = "";
+        let licenseNotice = "";
+
+        if (answers.license !== "") {
+            licenseBadge = `![License](https://img.shields.io/badge/license-${answers.license}-blue.svg)\n\n`;
+            licenseNotice = `This application is covered under the ${answers.license} license.\n\n`;
+        }
+
+        readme += `## License \n\n ${licenseBadge} ${licenseNotice}`;
+
+        readme += `##Questions\n\n`;
+        readme += `If you have any questions or comments about the application, please contact me at `;
+        readme += `${answers.email}. You can also visit my Github profile (https://github.com/${answers.githubUsername})`
+        readme += `to see my other projects.\n\n`
+
+        fs.writeFile("README.md", readme, (err) => {
+            if (err) throw err;
+            console.log("README.md file has been generated!");
+        });
     });
-  });
